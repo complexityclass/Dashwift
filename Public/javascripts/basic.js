683b304c72686376f2fd790c35471e33f5f73252
Popov Valery <v.popov@rambler-co.ru>
2016-01-30 18:29:03 +0300

    added babel to use jsx

--- Public/javascripts/basic.js
+++ Public/javascripts/basic.js
@@ -1,5 +1,4 @@
-var child1 = React.createElement('li', null, 'First Text Content');
-var child2 = React.createElement('li', null, 'Second Text Content');
-var root = React.createElement('ul', { className: 'my-list' }, child1, child2);
-
-ReactDOM.render(root, document.getElementById('content'));
+ReactDOM.render(
+  <h1>Hello react!</h1>,
+  document.getElementById('content')
+);
